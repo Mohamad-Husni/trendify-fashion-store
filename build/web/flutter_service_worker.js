f339c88c87561fa5ffe64cf6e331c42b138f1a6b
Mohamad-Husni <mmhusnimohamad@gmail.com>
2026-05-03 11:45:07 +0530

    Fix Firebase Auth JavaScript error - add comprehensive error handling to auth state changes

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "acca2531556559607ba7584670c8fbc4
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "49fa959ece96ae0a8685dca36fa77676",
+"flutter_bootstrap.js": "dd16e426c25fa4e48c49dfbef57bd472",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
