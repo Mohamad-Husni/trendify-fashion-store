6919b27751fd11545ddb800992f3c2b32bf85b9f
Mohamad-Husni <mmhusnimohamad@gmail.com>
2026-05-03 12:02:56 +0530

    Rebuild web app for GitHub Pages

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "acca2531556559607ba7584670c8fbc4
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "fb93ab152e6edf9cc0033108ca09fee4",
+"flutter_bootstrap.js": "876b417063f6d4f169642bf7c5812fa2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
